16b28a9bf774a72a2b5b339fb7d9863258150936
kyriest <freigilsonbrasil@gmail.com>
2026-04-02 01:19:34 -0300

    fix(types): augment react/jsx-runtime to resolve Three.js intrinsic elements error on React 19

--- apps/interno/src/vite-env.d.ts
+++ apps/interno/src/vite-env.d.ts
@@ -1,8 +1,23 @@
 /// <reference types="vite/client" />
+
 import { ThreeElements } from '@react-three/fiber'
 
-declare global {
+declare module 'react/jsx-runtime' {
   namespace JSX {
     interface IntrinsicElements extends ThreeElements {}
+  }
+}
+
+declare module 'react/jsx-dev-runtime' {
+  namespace JSX {
+    interface IntrinsicElements extends ThreeElements {}
+  }
+}
+
+declare global {
+  namespace React {
+    namespace JSX {
+      interface IntrinsicElements extends ThreeElements {}
+    }
   }
 }
