eb7a4383bdfe2d70c001ac28c04d3bb21e37c9bb
kyriest <freigilsonbrasil@gmail.com>
2026-04-16 07:37:51 -0300

    feat: inject GTM tracking scripts in root layout

--- apps/catalogo/src/app/layout.tsx
+++ apps/catalogo/src/app/layout.tsx
@@ -142,8 +142,27 @@ export default function RootLayout({
                 <link rel="preload" href="/hero-cheese/pao_left.png" as="image" />
                 <link rel="preload" href="/hero-cheese/pao_right.png" as="image" />
                 <link rel="preload" href="/hero-cheese/cheese.png" as="image" />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID || 'GTM-W6CXZC3R'}');
+                        `
+                    }}
+                />
             </head>
             <body className="font-body bg-mont-cream text-mont-espresso antialiased min-h-screen">
+                <noscript>
+                    <iframe
+                        src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID || 'GTM-W6CXZC3R'}`}
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    />
+                </noscript>
                 <PreloaderDismiss />
                 {children}
             </body>
